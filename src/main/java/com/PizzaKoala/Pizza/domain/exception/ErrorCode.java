8c496d3af43f1eed0202bfd0b57410acc73117f2
seuem <homlesssnail@gmail.com>
2024-09-06 00:12:47 +0900

    follow feature- entity & followAUser API

--- src/main/java/com/PizzaKoala/Pizza/domain/exception/ErrorCode.java
+++ src/main/java/com/PizzaKoala/Pizza/domain/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     DUPLICATED_EMAIL_ADDRESS(HttpStatus.CONFLICT,"The email already exists."),
     DUPLICATED_NICKNAME(HttpStatus.CONFLICT,"The nickname already exists."),
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,"The member does not exist."),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,"The user does not exist."),
+    FOLLOWING_NOT_FOUND(HttpStatus.NOT_FOUND, "The user you are trying to follow does not exist"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,"The password is invalid."),
     IMAGE_UPLOAD_REQUIRED(HttpStatus.BAD_REQUEST,"Photo upload is required"),
     ONE_TO_FIVE_IMAGES_ARE_REQUIRED(HttpStatus.BAD_REQUEST, "You must upload between 1 and 5 images"),
@@ -17,6 +18,7 @@ public enum ErrorCode {
     POST_NOT_FOUND(HttpStatus.NOT_FOUND,"The Post was not found."),
     INVALID_PERMISSION(HttpStatus.UNAUTHORIZED,"Permission is invalid."),
     ALREADY_LIKED(HttpStatus.CONFLICT,"User has already liked the post."),
+    ALREADY_FOLLOWED(HttpStatus.CONFLICT,"User has already followed the account."),
     ALREADY_UNLIKED(HttpStatus.CONFLICT,"User has already unliked the post."),
     LIKE_NOT_FOUND(HttpStatus.NOT_FOUND,"User hasn't liked the post."),
     FILE_NAME_NOT_FOUND(HttpStatus.NOT_FOUND,"The file name not found."),
